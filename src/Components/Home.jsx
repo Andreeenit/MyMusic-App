95b8184a9937177c21e74b0c3f48f16d138d48fb
Andreeenit <andremorix@hotmail.com>
2025-05-30 16:19:40 +0200

    Commit

---
+++ src/Components/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Home = () => {
+    return (
+        <div className='container'>
+            <h1>My Music</h1>
+            <h2>Utvecklare: Andreeenit</h2>
+        </div>
+    )
+}
+
+export default Home
